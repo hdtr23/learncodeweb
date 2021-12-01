9d46b5b88b5ae4e2e2395568a7bb7488dafc2492
hdtr <hien.td192833@sis.hust.edu.vn>
2021-12-01 23:37:06 +0700

    hoc code web

---
+++ src/config/connectDB.js
@@ -0,0 +1,16 @@
+const {Sequelize} = require('sequelize');
+
+const sequelize = new Sequelize('hdtr0', 'root', 'Hientd192833',{
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+let connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
+module.exports = connectDB;
